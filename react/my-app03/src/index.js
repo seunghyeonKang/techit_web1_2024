1427a491597a5a344d438291f3d37aaf2b01a40d
seunghyeonKang <number42isuser@gmail.com>
2024-06-18 23:19:13 +0900

    24.06.18_리액트 기초

--- react/my-app03/src/index.js
+++ react/my-app03/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // 01. JSX
 import Date from "./Exam01-Date"; // 02. 시간 표시
-import Exam02 from "./Exam02-List"; // 03. 리스트 렌더링
+import List from "./Exam02-List"; // 03. 리스트 렌더링
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
