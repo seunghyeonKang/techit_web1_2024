f203543a545b8cbe0bff398d625ba65c2732b32e
seunghyeonKang <number42isuser@gmail.com>
2024-07-12 23:28:29 +0900

    24.07.12_module.css

---
+++ react/chapter08_1/my-app/src/App.js
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+export default App;
---
+++ react/chapter08_1/my-app/src/components/Qna.jsx
@@ -0,0 +1,14 @@
+import styles from "./Qna.module.css";
+
+export function Qna() {
+  return (
+    <>
+      <h1 className={styles.box}>Qna에 대한 내용입니다!</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae
+        sunt ipsam voluptatibus provident reprehenderit amet iste, maxime
+        ducimus fugiat?
+      </p>
+    </>
+  );
+}
